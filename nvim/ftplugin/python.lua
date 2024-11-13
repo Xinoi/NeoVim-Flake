97983b36d78c309332eb86daf6223b9bfbebf1bf
Johannes Zaunrith <zaunrithj@gmail.com>
2024-11-13 13:15:25 +0100

    removed --stdio

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -16,7 +16,7 @@ local root_files = {
 
 vim.lsp.start {
   name = 'pyright',
-  cmd = {pyright_cmd, '--stdio'},
+  cmd = {pyright_cmd},
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
   settings = {
