28ad317c3df11b39e79bd9bdb945cab7f7fbdbbe
Johannes Zaunrith <zaunrithj@gmail.com>
2024-11-13 12:53:15 +0100

    added c lsp

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -5,5 +5,18 @@ if vim.fn.executable(clangd_cmd) ~= 1 then
 end
 
 local root_files = {
-  '.clangd'
+  '.clangd',
+  '.clang-tidy',
+  '.clang-format',
+  'compile_commands.json',
+  'compile_flags.txt',
+  'build',
+  '.git',
+}
+
+vim.lsp.start {
+  name = 'clangd',
+  cmd = { clangd_cmd },
+  root_dir = vim.fs.dirname(vim.fs.find(root_files, {upward = true})[1]),
+  capabilities = require('user.lsp').make_client_capabilities(),
 }
