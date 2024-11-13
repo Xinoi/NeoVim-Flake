0572ead31e5aceb5ae5b181c4a881f5ccfc08ee5
Johannes Zaunrith <zaunrithj@gmail.com>
2024-11-13 13:20:00 +0100

    ca

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,4 +1,4 @@
-local pyright_cmd = 'pyright'
+local pyright_cmd = 'pyright-langserver'
 
 if vim.fn.executable(pyright_cmd) ~= 1 then
   return
@@ -16,7 +16,7 @@ local root_files = {
 
 vim.lsp.start {
   name = 'pyright',
-  cmd = {pyright_cmd},
+  cmd = {pyright_cmd, '--stdio'},
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
   settings = {
