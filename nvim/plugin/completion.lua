b9ebf8b237f100fe2a0dd8826483b3c6566685ae
Johannes Zaunrith <zaunrithj@gmail.com>
2025-01-21 18:41:59 +0100

    sd

--- nvim/plugin/completion.lua
+++ nvim/plugin/completion.lua
@@ -99,6 +99,7 @@ cmp.setup {
   },
   sources = cmp.config.sources {
     -- The insertion order influences the priority of the sources
+    { name = 'omni' },
     { name = 'nvim_lsp', keyword_length = 3 },
     { name = 'nvim_lsp_signature_help', keyword_length = 3 },
     { name = 'buffer' },
