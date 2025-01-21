b3f44571529e6a1675732541ff512c0a29a37f91
Johannes Zaunrith <zaunrithj@gmail.com>
2025-01-21 18:34:46 +0100

    add ultisnips

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -33,6 +33,7 @@ with final.pkgs.lib; let
     # nvim-cmp (autocompletion) and extensions
     nvim-cmp # https://github.com/hrsh7th/nvim-cmp
     cmp-omni
+    ultisnips
     cmp_luasnip # snippets autocompletion extension for nvim-cmp | https://github.com/saadparwaiz1/cmp_luasnip/
     lspkind-nvim # vscode-like LSP pictograms | https://github.com/onsails/lspkind.nvim/
     cmp-nvim-lsp # LSP as completion source | https://github.com/hrsh7th/cmp-nvim-lsp/
